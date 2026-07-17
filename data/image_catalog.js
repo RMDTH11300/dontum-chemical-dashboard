4df93911b539d2dee6ef0e1c670a27745de34712
RMDTH11300 <mulberrybaimon25@gmail.com>
2026-07-17 10:21:21 +0700

    เพิ่มภาพ SDS และหน้ารายละเอียดสารเคมี

---
+++ data/image_catalog.js
@@ -0,0 +1,578 @@
+window.SDS_IMAGE_CATALOG = [
+  {
+    "id": "001",
+    "title": "10% Sodium Hypochlorite (Maxwhite)",
+    "image": "assets/chemical-images/chemical-001.webp",
+    "sdsCode": "SDS-CHEM-001"
+  },
+  {
+    "id": "002",
+    "title": "17% EDTA (MD-Solution EDTA)",
+    "image": "assets/chemical-images/chemical-002.webp",
+    "sdsCode": "SDS-CHEM-002"
+  },
+  {
+    "id": "003",
+    "title": "Detergent Liquid",
+    "image": "assets/chemical-images/chemical-003.webp",
+    "sdsCode": "SDS-CHEM-003"
+  },
+  {
+    "id": "004",
+    "title": "Hydrogen Peroxide 3%",
+    "image": "assets/chemical-images/chemical-004.webp",
+    "sdsCode": "SDS-CHEM-004"
+  },
+  {
+    "id": "005",
+    "title": "Alcohol 70% (Ethanol 70%)",
+    "image": "assets/chemical-images/chemical-005.webp",
+    "sdsCode": "SDS-CHEM-005"
+  },
+  {
+    "id": "006",
+    "title": "Glutaraldehyde 2%",
+    "image": "assets/chemical-images/chemical-006.webp",
+    "sdsCode": "SDS-CHEM-006"
+  },
+  {
+    "id": "007",
+    "title": "Sodium Hypochlorite 6%",
+    "image": "assets/chemical-images/chemical-007.webp",
+    "sdsCode": "SDS-CHEM-007"
+  },
+  {
+    "id": "008",
+    "title": "Hand Sanitizer (Ethanol 75%)",
+    "image": "assets/chemical-images/chemical-008.webp",
+    "sdsCode": "SDS-CHEM-008"
+  },
+  {
+    "id": "009",
+    "title": "Peracetic Acid 0.2%",
+    "image": "assets/chemical-images/chemical-009.webp",
+    "sdsCode": "SDS-CHEM-009"
+  },
+  {
+    "id": "010",
+    "title": "Povidone-Iodine 10%",
+    "image": "assets/chemical-images/chemical-010.webp",
+    "sdsCode": "SDS-CHEM-010"
+  },
+  {
+    "id": "011",
+    "title": "Chlorhexidine Gluconate 4%",
+    "image": "assets/chemical-images/chemical-011.webp",
+    "sdsCode": "SDS-CHEM-011"
+  },
+  {
+    "id": "012",
+    "title": "Hydrogen Peroxide 3%",
+    "image": "assets/chemical-images/chemical-012.webp",
+    "sdsCode": "SDS-CHEM-012"
+  },
+  {
+    "id": "013",
+    "title": "Formaldehyde 37%",
+    "image": "assets/chemical-images/chemical-013.webp",
+    "sdsCode": "SDS-CHEM-013"
+  },
+  {
+    "id": "014",
+    "title": "Silver Nitrate 10%",
+    "image": "assets/chemical-images/chemical-014.webp",
+    "sdsCode": "SDS-CHEM-014"
+  },
+  {
+    "id": "015",
+    "title": "Glutaraldehyde 2%",
+    "image": "assets/chemical-images/chemical-015.webp",
+    "sdsCode": "SDS-CHEM-015"
+  },
+  {
+    "id": "016",
+    "title": "Acetic Acid 5%",
+    "image": "assets/chemical-images/chemical-016.webp",
+    "sdsCode": "SDS-CHEM-016"
+  },
+  {
+    "id": "017",
+    "title": "Sodium Hydroxide 10%",
+    "image": "assets/chemical-images/chemical-017.webp",
+    "sdsCode": "SDS-CHEM-017"
+  },
+  {
+    "id": "018",
+    "title": "Detergent (Neutral)",
+    "image": "assets/chemical-images/chemical-018.webp",
+    "sdsCode": "SDS-CHEM-018"
+  },
+  {
+    "id": "019",
+    "title": "Isopropyl Alcohol 70%",
+    "image": "assets/chemical-images/chemical-019.webp",
+    "sdsCode": "SDS-CHEM-019"
+  },
+  {
+    "id": "020",
+    "title": "Phenol 5% Solution",
+    "image": "assets/chemical-images/chemical-020.webp",
+    "sdsCode": "SDS-CHEM-020"
+  },
+  {
+    "id": "021",
+    "title": "Sodium Hypochlorite 6%",
+    "image": "assets/chemical-images/chemical-021.webp",
+    "sdsCode": "SDS-CHEM-021"
+  },
+  {
+    "id": "022",
+    "title": "Quaternary Ammonium Compound",
+    "image": "assets/chemical-images/chemical-022.webp",
+    "sdsCode": "SDS-CHEM-022"
+  },
+  {
+    "id": "023",
+    "title": "Hydrogen Peroxide 30%",
+    "image": "assets/chemical-images/chemical-023.webp",
+    "sdsCode": "SDS-CHEM-023"
+  },
+  {
+    "id": "024",
+    "title": "Sodium Bicarbonate",
+    "image": "assets/chemical-images/chemical-024.webp",
+    "sdsCode": "SDS-CHEM-024"
+  },
+  {
+    "id": "025",
+    "title": "Acetic Acid (Glacial)",
+    "image": "assets/chemical-images/chemical-025.webp",
+    "sdsCode": "SDS-CHEM-025"
+  },
+  {
+    "id": "026",
+    "title": "Sodium Thiosulfate",
+    "image": "assets/chemical-images/chemical-026.webp",
+    "sdsCode": "SDS-CHEM-026"
+  },
+  {
+    "id": "027",
+    "title": "Formaldehyde 37% Solution",
+    "image": "assets/chemical-images/chemical-027.webp",
+    "sdsCode": "SDS-CHEM-027"
+  },
+  {
+    "id": "028",
+    "title": "Glutaraldehyde 2% Solution",
+    "image": "assets/chemical-images/chemical-028.webp",
+    "sdsCode": "SDS-CHEM-028"
+  },
+  {
+    "id": "029",
+    "title": "Benzalkonium Chloride 10% Solution",
+    "image": "assets/chemical-images/chemical-029.webp",
+    "sdsCode": "SDS-CHEM-029"
+  },
+  {
+    "id": "030",
+    "title": "Iodine Tincture 2% w/v",
+    "image": "assets/chemical-images/chemical-030.webp",
+    "sdsCode": "SDS-CHEM-030"
+  },
+  {
+    "id": "031",
+    "title": "Ethanol 70% v/v",
+    "image": "assets/chemical-images/chemical-031.webp",
+    "sdsCode": "SDS-CHEM-031"
+  },
+  {
+    "id": "032",
+    "title": "Sodium Hypochlorite 5% w/v (Bleach)",
+    "image": "assets/chemical-images/chemical-032.webp",
+    "sdsCode": "SDS-CHEM-032"
+  },
+  {
+    "id": "033",
+    "title": "Hydrogen Peroxide 3% w/v",
+    "image": "assets/chemical-images/chemical-033.webp",
+    "sdsCode": "SDS-CHEM-033"
+  },
+  {
+    "id": "034",
+    "title": "Sodium Bicarbonate",
+    "image": "assets/chemical-images/chemical-034.webp",
+    "sdsCode": "SDS-CHEM-034"
+  },
+  {
+    "id": "035",
+    "title": "Silver Nitrate Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-035.webp",
+    "sdsCode": "SDS-CHEM-035"
+  },
+  {
+    "id": "036",
+    "title": "Potassium Permanganate",
+    "image": "assets/chemical-images/chemical-036.webp",
+    "sdsCode": "SDS-CHEM-036"
+  },
+  {
+    "id": "037",
+    "title": "Acetic Acid Glacial",
+    "image": "assets/chemical-images/chemical-037.webp",
+    "sdsCode": "SDS-CHEM-037"
+  },
+  {
+    "id": "038",
+    "title": "Sodium Hydroxide 10% w/v Solution",
+    "image": "assets/chemical-images/chemical-038.webp",
+    "sdsCode": "SDS-CHEM-038"
+  },
+  {
+    "id": "039",
+    "title": "Hypochlorous Acid Solution 0.02%",
+    "image": "assets/chemical-images/chemical-039.webp",
+    "sdsCode": "SDS-CHEM-039"
+  },
+  {
+    "id": "040",
+    "title": "Iso Propyl Alcohol 70% v/v",
+    "image": "assets/chemical-images/chemical-040.webp",
+    "sdsCode": "SDS-CHEM-040"
+  },
+  {
+    "id": "041",
+    "title": "Formaldehyde Solution 37% w/v",
+    "image": "assets/chemical-images/chemical-041.webp",
+    "sdsCode": "SDS-CHEM-041"
+  },
+  {
+    "id": "042",
+    "title": "Glutaraldehyde Solution 2% w/v",
+    "image": "assets/chemical-images/chemical-042.webp",
+    "sdsCode": "SDS-CHEM-042"
+  },
+  {
+    "id": "043",
+    "title": "Ethanol 95% w/v",
+    "image": "assets/chemical-images/chemical-043.webp",
+    "sdsCode": "SDS-CHEM-043"
+  },
+  {
+    "id": "044",
+    "title": "Peroxyacetic Acid 5% w/v Solution",
+    "image": "assets/chemical-images/chemical-044.webp",
+    "sdsCode": "SDS-CHEM-044"
+  },
+  {
+    "id": "045",
+    "title": "Sodium Hypochlorite Solution 5% w/v",
+    "image": "assets/chemical-images/chemical-045.webp",
+    "sdsCode": "SDS-CHEM-045"
+  },
+  {
+    "id": "046",
+    "title": "Hydrogen Peroxide Solution 3% w/v",
+    "image": "assets/chemical-images/chemical-046.webp",
+    "sdsCode": "SDS-CHEM-046"
+  },
+  {
+    "id": "047",
+    "title": "Citric Acid Monohydrate",
+    "image": "assets/chemical-images/chemical-047.webp",
+    "sdsCode": "SDS-CHEM-047"
+  },
+  {
+    "id": "048",
+    "title": "Ammonia Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-048.webp",
+    "sdsCode": "SDS-CHEM-048"
+  },
+  {
+    "id": "049",
+    "title": "Sodium Bicarbonate",
+    "image": "assets/chemical-images/chemical-049.webp",
+    "sdsCode": "SDS-CHEM-049"
+  },
+  {
+    "id": "050",
+    "title": "Sodium Thiosulfate Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-050.webp",
+    "sdsCode": "SDS-CHEM-050"
+  },
+  {
+    "id": "051",
+    "title": "Sodium Percarbonate",
+    "image": "assets/chemical-images/chemical-051.webp",
+    "sdsCode": "SDS-CHEM-051"
+  },
+  {
+    "id": "052",
+    "title": "Isopropyl Alcohol 70% v/v",
+    "image": "assets/chemical-images/chemical-052.webp",
+    "sdsCode": "SDS-CHEM-052"
+  },
+  {
+    "id": "053",
+    "title": "Boric Acid",
+    "image": "assets/chemical-images/chemical-053.webp",
+    "sdsCode": "SDS-CHEM-053"
+  },
+  {
+    "id": "054",
+    "title": "Formaldehyde Solution 37–40% w/v",
+    "image": "assets/chemical-images/chemical-054.webp",
+    "sdsCode": "SDS-CHEM-054"
+  },
+  {
+    "id": "055",
+    "title": "Sodium Nitrite",
+    "image": "assets/chemical-images/chemical-055.webp",
+    "sdsCode": "SDS-CHEM-055"
+  },
+  {
+    "id": "056",
+    "title": "Acetic Acid Glacial ≥99.5%",
+    "image": "assets/chemical-images/chemical-056.webp",
+    "sdsCode": "SDS-CHEM-056"
+  },
+  {
+    "id": "057",
+    "title": "Hydrochloric Acid 37% w/w",
+    "image": "assets/chemical-images/chemical-057.webp",
+    "sdsCode": "SDS-CHEM-057"
+  },
+  {
+    "id": "058",
+    "title": "Sodium Hypochlorite Solution 5–6% w/v",
+    "image": "assets/chemical-images/chemical-058.webp",
+    "sdsCode": "SDS-CHEM-058"
+  },
+  {
+    "id": "059",
+    "title": "Sodium Hydroxide Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-059.webp",
+    "sdsCode": "SDS-CHEM-059"
+  },
+  {
+    "id": "060",
+    "title": "Glacial Acetic Acid ≥99.5%",
+    "image": "assets/chemical-images/chemical-060.webp",
+    "sdsCode": "SDS-CHEM-060"
+  },
+  {
+    "id": "061",
+    "title": "Sodium Thiosulfate Pentahydrate",
+    "image": "assets/chemical-images/chemical-061.webp",
+    "sdsCode": "SDS-CHEM-061"
+  },
+  {
+    "id": "062",
+    "title": "Ammonia Solution 25% w/v",
+    "image": "assets/chemical-images/chemical-062.webp",
+    "sdsCode": "SDS-CHEM-062"
+  },
+  {
+    "id": "063",
+    "title": "Potassium Permanganate",
+    "image": "assets/chemical-images/chemical-063.webp",
+    "sdsCode": "SDS-CHEM-063"
+  },
+  {
+    "id": "064",
+    "title": "Sodium Bicarbonate",
+    "image": "assets/chemical-images/chemical-064.webp",
+    "sdsCode": "SDS-CHEM-064"
+  },
+  {
+    "id": "065",
+    "title": "Hydrogen Peroxide Solution 3% w/v",
+    "image": "assets/chemical-images/chemical-065.webp",
+    "sdsCode": "SDS-CHEM-065"
+  },
+  {
+    "id": "066",
+    "title": "Sodium Chloride Solution 0.9% w/v",
+    "image": "assets/chemical-images/chemical-066.webp",
+    "sdsCode": "SDS-CHEM-066"
+  },
+  {
+    "id": "067",
+    "title": "Formaldehyde Solution 37% w/v",
+    "image": "assets/chemical-images/chemical-067.webp",
+    "sdsCode": "SDS-CHEM-067"
+  },
+  {
+    "id": "068",
+    "title": "Acetone",
+    "image": "assets/chemical-images/chemical-068.webp",
+    "sdsCode": "SDS-CHEM-068"
+  },
+  {
+    "id": "069",
+    "title": "Isopropyl Alcohol 70% w/v",
+    "image": "assets/chemical-images/chemical-069.webp",
+    "sdsCode": "SDS-CHEM-069"
+  },
+  {
+    "id": "070",
+    "title": "Sodium Hypochlorite Solution 5% w/v",
+    "image": "assets/chemical-images/chemical-070.webp",
+    "sdsCode": "SDS-CHEM-070"
+  },
+  {
+    "id": "071",
+    "title": "Glutaraldehyde Solution 2% w/v",
+    "image": "assets/chemical-images/chemical-071.webp",
+    "sdsCode": "SDS-CHEM-071"
+  },
+  {
+    "id": "072",
+    "title": "Peroxyacetic Acid Solution 0.2% w/v",
+    "image": "assets/chemical-images/chemical-072.webp",
+    "sdsCode": "SDS-CHEM-072"
+  },
+  {
+    "id": "073",
+    "title": "Hydrochloric Acid 10% w/v",
+    "image": "assets/chemical-images/chemical-073.webp",
+    "sdsCode": "SDS-CHEM-073"
+  },
+  {
+    "id": "074",
+    "title": "Ammonia Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-074.webp",
+    "sdsCode": "SDS-CHEM-074"
+  },
+  {
+    "id": "075",
+    "title": "Sodium Hydroxide Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-075.webp",
+    "sdsCode": "SDS-CHEM-075"
+  },
+  {
+    "id": "076",
+    "title": "Ethanol 70% v/v",
+    "image": "assets/chemical-images/chemical-076.webp",
+    "sdsCode": "SDS-CHEM-076"
+  },
+  {
+    "id": "077",
+    "title": "Hydrogen Peroxide Solution 3% w/v",
+    "image": "assets/chemical-images/chemical-077.webp",
+    "sdsCode": "SDS-CHEM-077"
+  },
+  {
+    "id": "078",
+    "title": "Sodium Bicarbonate",
+    "image": "assets/chemical-images/chemical-078.webp",
+    "sdsCode": "SDS-CHEM-078"
+  },
+  {
+    "id": "079",
+    "title": "Calcium Hypochlorite 65% w/w",
+    "image": "assets/chemical-images/chemical-079.webp",
+    "sdsCode": "SDS-CHEM-079"
+  },
+  {
+    "id": "080",
+    "title": "Sodium Lauryl Sulfate (SLS)",
+    "image": "assets/chemical-images/chemical-080.webp",
+    "sdsCode": "SDS-CHEM-080"
+  },
+  {
+    "id": "081",
+    "title": "Benzalkonium Chloride Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-081.webp",
+    "sdsCode": "SDS-CHEM-081"
+  },
+  {
+    "id": "082",
+    "title": "Sodium Hypochlorite Solution 5% w/v",
+    "image": "assets/chemical-images/chemical-082.webp",
+    "sdsCode": "SDS-CHEM-082"
+  },
+  {
+    "id": "083",
+    "title": "Glutaraldehyde Solution 2% w/v",
+    "image": "assets/chemical-images/chemical-083.webp",
+    "sdsCode": "SDS-CHEM-083"
+  },
+  {
+    "id": "084",
+    "title": "Formalin 10% w/v",
+    "image": "assets/chemical-images/chemical-084.webp",
+    "sdsCode": "SDS-CHEM-084"
+  },
+  {
+    "id": "085",
+    "title": "Acetic Acid Glacial 99%",
+    "image": "assets/chemical-images/chemical-085.webp",
+    "sdsCode": "SDS-CHEM-085"
+  },
+  {
+    "id": "086",
+    "title": "Ammonium Hydroxide Solution 25% w/v",
+    "image": "assets/chemical-images/chemical-086.webp",
+    "sdsCode": "SDS-CHEM-086"
+  },
+  {
+    "id": "087",
+    "title": "Ethanol (Ethyl Alcohol) 70% v/v",
+    "image": "assets/chemical-images/chemical-087.webp",
+    "sdsCode": "SDS-CHEM-087"
+  },
+  {
+    "id": "088",
+    "title": "Isopropyl Alcohol 70% v/v",
+    "image": "assets/chemical-images/chemical-088.webp",
+    "sdsCode": "SDS-CHEM-088"
+  },
+  {
+    "id": "089",
+    "title": "Sodium Bicarbonate >99%",
+    "image": "assets/chemical-images/chemical-089.webp",
+    "sdsCode": "SDS-CHEM-089"
+  },
+  {
+    "id": "090",
+    "title": "Hydrochloric Acid Solution 37% w/w",
+    "image": "assets/chemical-images/chemical-090.webp",
+    "sdsCode": "SDS-CHEM-090"
+  },
+  {
+    "id": "091",
+    "title": "Sodium Thiosulfate Solution 10% w/v",
+    "image": "assets/chemical-images/chemical-091.webp",
+    "sdsCode": "SDS-CHEM-091"
+  },
+  {
+    "id": "092",
+    "title": "Potassium Permanganate Solution 0.1% w/v",
+    "image": "assets/chemical-images/chemical-092.webp",
+    "sdsCode": "SDS-CHEM-092"
+  },
+  {
+    "id": "093",
+    "title": "Sodium Hypochlorite Solution 5% w/v",
+    "image": "assets/chemical-images/chemical-093.webp",
+    "sdsCode": "SDS-CHEM-093"
+  },
+  {
+    "id": "094",
+    "title": "Hydrogen Peroxide Solution 3% w/v",
+    "image": "assets/chemical-images/chemical-094.webp",
+    "sdsCode": "SDS-CHEM-094"
+  },
+  {
+    "id": "095",
+    "title": "Formaldehyde Solution 37% w/v",
+    "image": "assets/chemical-images/chemical-095.webp",
+    "sdsCode": "SDS-CHEM-095"
+  },
+  {
+    "id": "096",
+    "title": "Glutaraldehyde Solution 2.5% w/v",
+    "image": "assets/chemical-images/chemical-096.webp",
+    "sdsCode": "SDS-CHEM-096"
+  }
+];
